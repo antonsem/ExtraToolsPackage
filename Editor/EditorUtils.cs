03eaa5570ffb0f796d92a23bc00dfd20c523281b
Anton <semchenkoanton@pm.com>
2023-06-18 18:11:52 +0200

    - unity version update - code style update - quality of life update

---
+++ Editor/EditorUtils.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace ExtraTools.Editor
+{
+	public static class EditorUtils
+	{
+		/// <summary>
+		/// Credit goes to FreCre (https://assetstore.unity.com/publishers/7617).
+		/// Stumbled across this shortcut on twitter and decided to add
+		/// to the Extra Tools. The original free asset package created by FreCre can be
+		/// downloaded from the Asset Store: https://assetstore.unity.com/packages/tools/easyshortcutlockinspector-23579
+		/// </summary>
+		[MenuItem("Extra Tools/Toggle Inspector Lock %l")]
+		private static void ToggleInspectorLock()
+		{
+			ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+			ActiveEditorTracker.sharedTracker.ForceRebuild();
+		}
+
+		/// <summary>
+		/// Credit goes to KirillKuzyk from answers.unity.com.
+		/// Got this from here: https://answers.unity.com/questions/707636/clear-console-window.html
+		/// For more information about MenuItem see documentation: https://docs.unity3d.com/ScriptReference/MenuItem.html
+		/// </summary>
+		[MenuItem("Extra Tools/Clear Console %q")] // CTRL + Q
+		private static void ClearConsole()
+		{
+			Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+			Type type = assembly.GetType("UnityEditor.LogEntries");
+			MethodInfo method = type.GetMethod("Clear");
+			method?.Invoke(new object(), null);
+		}
+
+		/// <summary>
+		/// Credit goes to coeing from answers.unity.com
+		/// Got this from here: https://answers.unity.com/questions/43422/how-to-implement-show-in-explorer.html
+		/// PS: A similar function was added to the Shortcut editor in the 2019.1. Still, this
+		/// can be used to open a specific folder (or an asset), not the one you clicked on in the Projects tab
+		/// </summary>
+		[MenuItem("Extra Tools/Open Project Folder %e")] // CTRL + E
+		public static void ShowExplorer()
+		{
+			string path = Directory.GetParent(Application.dataPath).FullName;
+			EditorUtility.RevealInFinder(path);
+		}
+
+		[MenuItem("Extra Tools/Open Persistant Data Folder %t")] // CTRL + T
+		public static void ShowPersistantData()
+		{
+			string path = Directory.GetParent(Application.persistentDataPath).FullName;
+			EditorUtility.RevealInFinder(path);
+		}
+
+		private const BindingFlags PRIVATE_PUBLIC_INSTANCE =
+			BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
+
+		public static object GetValue(this SerializedProperty property)
+		{
+			object obj = property.serializedObject.targetObject;
+			Type type = obj.GetType();
+			FieldInfo field = type.GetField(property.propertyPath, PRIVATE_PUBLIC_INSTANCE);
+			return field?.GetValue(obj);
+		}
+	}
+}
--- Editor/RequiredFieldDrawer.cs
+++ Editor/RequiredFieldDrawer.cs
@@ -3,30 +3,36 @@ using UnityEngine;
 
 namespace ExtraTools.Editor
 {
-    /// <summary>
-    /// Credit for this tip goes to Rodrigo Devora
-    /// Got it from the twitter: https://twitter.com/Rodrigo_Devora/status/1204031607583264769
-    /// </summary>
-    [CustomPropertyDrawer(typeof(RequiredField))]
-    public class RequiredFieldDrawer : PropertyDrawer
-    {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            RequiredField field = attribute as RequiredField;
+	/// <summary>
+	/// Credit for this tip goes to Rodrigo Devora
+	/// Got it from the twitter: https://twitter.com/Rodrigo_Devora/status/1204031607583264769
+	/// </summary>
+	[CustomPropertyDrawer(typeof(RequiredField))]
+	public class RequiredFieldDrawer : PropertyDrawer
+	{
+		private const string ERROR =
+			"Required field <color=red>{0}</color> is not assigned on <color=blue>{1}</color>!";
 
-            if (property.objectReferenceValue == null)
-            {
-                if (field.debugError)
-                    Debug.LogErrorFormat(property.serializedObject.targetObject,
-                        "Required field <color=red>{0}</color> is not assigned on <color=blue>{1}</color>!", label.text,
-                        property.serializedObject.targetObject);
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			var field = attribute as RequiredField;
 
-                GUI.color = field.color;
-                EditorGUI.PropertyField(position, property, label);
-                GUI.color = Color.white;
-            }
-            else
-                EditorGUI.PropertyField(position, property, label);
-        }
-    }
+			if (!property.GetValue().Equals(null))
+			{
+				EditorGUI.PropertyField(position, property, label);
+				return;
+			}
+
+			if (field.debugError)
+			{
+				Debug.LogErrorFormat(property.serializedObject.targetObject, ERROR, label.text,
+					property.serializedObject.targetObject);
+			}
+
+			var previousGUIColor = GUI.color;
+			GUI.color = field.color;
+			EditorGUI.PropertyField(position, property, label);
+			GUI.color = previousGUIColor;
+		}
+	}
 }
